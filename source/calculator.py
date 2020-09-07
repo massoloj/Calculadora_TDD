799b44476a4bc2099ecd3aa937f979dbb92225f2
julian <massolo.j@gmail.com>
2020-09-06 21:49:44 -0300

    Creacion de las clases para la calculadora y los tests

---
+++ source/calculator.py
@@ -0,0 +1,16 @@
+
+class Calculator:
+    def __init__(self):
+        pass
+
+    def add(self, value1, value2):
+        pass
+
+    def substract(self, value1, value2):
+        pass
+
+    def multiply(self, value1, value2):
+        pass
+
+    def divide(self, value1, value2):
+        pass
---
+++ tests/calculator_tester.py
@@ -0,0 +1,32 @@
+import unittest
+
+
+class CalculatorTester(unittest.TestCase):
+
+    def test_add_success(self):
+        pass
+
+    def test_add_fails(self):
+        pass
+
+    def test_substract_success(self):
+        pass
+
+    def test_substract_fails(self):
+        pass
+
+    def test_multiply_success(self):
+        pass
+
+    def test_multiply_fails(self):
+        pass
+
+    def test_divide_success(self):
+        pass
+
+    def test_multiply_fails(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
